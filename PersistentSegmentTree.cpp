f71a19b5f8f4c26879115c9810b692f921058ae0
Raghav Bansal <43716103+bansalraghav19@users.noreply.github.com>
2020-10-09 22:14:24 +0530

    Update PersistentSegmentTree.cpp

--- PersistentSegmentTree.cpp
+++ PersistentSegmentTree.cpp
@@ -2,7 +2,7 @@ struct Node {
 	int cnt = 0;
 	Node *l, *r;
 	Node(): cnt(0), l(nullptr), r(nullptr) {}
-	Node(Node* root): cnt(root->cnt + 1), l(nullptr), r(nullptr) {}
+	Node(Node* root, int value): cnt(root->cnt + value), l(nullptr), r(nullptr) {}
 	Node(Node* L, Node* R): cnt(0), l(L), r(R) {
 		if (l) { cnt += l->cnt; }
 		if (r) { cnt += r->cnt; }
@@ -16,27 +16,25 @@ struct Segtree {
 		int M = (L + R) / 2;
 		return new Node(Build(L, M), Build(M + 1, R));
 	}
-	Node* modify(Node* root, int L, int R, int pos) {
+	Node* modify(Node* root, int L, int R, int pos, int value) {
 		if (L == R) {
-			return new Node(root);
+			return new Node(root, value);
 		}
 		int M = (L + R) / 2;
 		if (pos <= M) {
-			return new Node(modify(root->l, L, M, pos), root->r);
+			return new Node(modify(root->l, L, M, pos, value), root->r);
 		} else {
-			return new Node(root->l, modify(root->r, M + 1, R, pos));
+			return new Node(root->l, modify(root->r, M + 1, R, pos, value));
 		}
 	}
-	int query(Node *right, Node *left, int L, int R, int k) {
-		if (L == R) {
-			return L;
+	int query(Node *root, int L, int R, int ql, int qr) {
+		if (L > qr or R < ql) {
+			return 0;
+		}
+		if (L >= ql and R <= qr) {
+			return root->cnt;
 		}
 		int M = (L + R) / 2;
-		int count = right->l->cnt - left->l->cnt;
-		if (count >= k) {
-			return query(right->l, left->l, L, M, k);
-		} else {
-			return query(right->r, left->r, M + 1, R, k - count);
-		}
+		return query(root->l, L, M, ql, qr) + query(root->r, M + 1, R, ql, qr);
 	}
 };
