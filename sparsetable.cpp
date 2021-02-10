087fa5daf66072380416a3b8b54522797809b093
Raghav Bansal <43716103+bansalraghav19@users.noreply.github.com>
2021-02-10 23:57:32 +0530

    Update sparsetable.cpp

--- sparsetable.cpp
+++ sparsetable.cpp
@@ -3,10 +3,9 @@ template<typename T>
 struct sparse_table {
   const int level = 19;
   int N;
-  using Node = array<int, 3>;
   vector<int> v;
   vector<int> floorlogs;
-  vector<vector<Node>> table;
+  vector<vector<int>> table;
   T func;
   sparse_table(T lambda) {
     func = lambda;
@@ -15,26 +14,25 @@ struct sparse_table {
     this->N = (int)v.size() - 1LL;
     this->v = v;
     floorlogs.resize(this->N + 1);
-    table.resize(this->N + 1, vector<Node>(level + 1));
+    table.resize(this->N + 1, vector<int>(level + 1));
     precompute();
     Modify();
   }
   void precompute() {
-    table[1][0][0] = table[1][0][1] = table[1][0][2] = v[1];
-    for (int i = 2; i <= N; i++) {
-      table[i][0][0] = table[i][0][1] = table[i][0][2] = v[i];
-      floorlogs[i] = floorlogs[i - 1] + !(i & (i - 1));
+    for (int i = 1; i <= N; i++) {
+      table[i][0] = v[i];
+      floorlogs[i] = floorlogs[i - 1] + !(!(i - 1) or i & (i - 1));
     }
   }
   void Modify() {
     for (int i = 1; i <= level; i++) {
       for (int j = 1; j + (1LL << i) - 1 <= N; j++) {
-        table[j][i][0] = func(table[j][i - 1][0], table[j + (1LL << (i - 1))][i - 1][0]);
+        table[j][i] = func(table[j][i - 1], table[j + (1LL << (i - 1))][i - 1]);
       }
     }
   }
   int Query(int l, int r) {
     int msb = floorlogs[r - l + 1];
-    return func(table[l][msb][0], table[r - (1LL << msb) + 1][msb][0]);
+    return func(table[l][msb], table[r - (1LL << msb) + 1][msb]);
   }
 };
