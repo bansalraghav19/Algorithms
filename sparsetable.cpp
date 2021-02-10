7eeafef879e96e040db8573fe3d4b5d9ddff2825
Raghav Bansal <43716103+bansalraghav19@users.noreply.github.com>
2021-02-10 23:08:36 +0530

    Update sparsetable.cpp

--- sparsetable.cpp
+++ sparsetable.cpp
@@ -1,16 +1,23 @@
+using FUN = function<int(int, int)>;
+template<typename T>
 struct sparse_table {
+  const int level = 19;
   int N;
-  const int level = 20;
   using Node = array<int, 3>;
   vector<int> v;
   vector<int> floorlogs;
   vector<vector<Node>> table;
+  T func;
+  sparse_table(T lambda) {
+    func = lambda;
+  }
   void init(vector<int> &v) {
     this->N = (int)v.size() - 1LL;
     this->v = v;
     floorlogs.resize(this->N + 1);
     table.resize(this->N + 1, vector<Node>(level + 1));
     precompute();
+    Modify();
   }
   void precompute() {
     table[1][0][0] = table[1][0][1] = table[1][0][2] = v[1];
@@ -19,37 +26,15 @@ struct sparse_table {
       floorlogs[i] = floorlogs[i - 1] + !(i & (i - 1));
     }
   }
-  void precomputemax() {
+  void Modify() {
     for (int i = 1; i <= level; i++) {
       for (int j = 1; j + (1LL << i) - 1 <= N; j++) {
-        table[j][i][0] = max(table[j][i - 1][0], table[j + (1LL << (i - 1))][i - 1][0]);
+        table[j][i][0] = func(table[j][i - 1][0], table[j + (1LL << (i - 1))][i - 1][0]);
       }
     }
   }
-  void precomputemin() {
-    for (int i = 1; i <= level; i++) {
-      for (int j = 1; j + (1LL << i) - 1 <= N; j++) {
-        table[j][i][1] = min(table[j][i - 1][1], table[j + (1LL << (i - 1))][i - 1][1]);
-      }
-    }
-  }
-  void precomputegcd() {
-    for (int i = 1; i <= level; i++) {
-      for (int j = 1; j + (1LL << i) - 1 <= N; j++) {
-        table[j][i][2] = __gcd(table[j][i - 1][2], table[j + (1LL << (i - 1))][i - 1][2]);
-      }
-    }
-  }
-  int getmax(int l, int r) {
+  int Query(int l, int r) {
     int msb = floorlogs[r - l + 1];
-    return max(table[l][msb][0], table[r - (1LL << msb) + 1][msb][0]);
-  }
-  int getmin(int l, int r) {
-    int msb = floorlogs[r - l + 1];
-    return min(table[l][msb][1], table[r - (1LL << msb) + 1][msb][1]);
-  }
-  int getgcd(int l, int r) {
-    int msb = floorlogs[r - l + 1];
-    return __gcd(table[l][msb][2], table[r - (1LL << msb) + 1][msb][2]);
+    return func(table[l][msb][0], table[r - (1LL << msb) + 1][msb][0]);
   }
 };
